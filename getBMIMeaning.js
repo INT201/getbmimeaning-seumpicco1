1a210c64f4c99f858df0b5da7123c172bf27781b
seumpicco1 <narawit.seum9@mail.kmutt.ac.th>
2022-09-04 15:28:12 +0700

    bmi

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,25 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  
+ return weight / (height*height)
 }
 function getBMIMeaning(weight, height) {
-  //code here
-}
-module.exports = getBMIMeaning
+  let BMI =calculateBMI(weight,height)
+ if(BMI < 18.5) {
+
+  return "Underweight"
+
+ }
+ else if (BMI >18.5 && BMI <=24.9){
+return "Normal weight"
+
+ }
+ else {
+
+  return "Over weight "
+ }
+};
+
+console.log(calculateBMI(70,170))
+console.log(getBMIMeaning(64,3))
